37cafe8df195d3a371002755765d663a4f01bfa8
Dan Macumber <dan.macumber@gmail.com>
2019-01-23 15:57:48 -0700

    Updates to add a better run_command method.  We have been trying to consolodate on a single implementation of this, extension gem could be a good place

--- lib/openstudio/extension/runner.rb
+++ lib/openstudio/extension/runner.rb
@@ -1,3 +1,6 @@
+require 'open3'
+require 'openstudio'
+
 module OpenStudio
   module Extension
     class Runner
@@ -7,6 +10,51 @@ module OpenStudio
         puts "Initializing runner with path: #{path}"
         @path = path
       end
+      
+      # DLM: not sure where this code should go
+      def get_run_env()
+        # blank out bundler and gem path modifications, will be re-setup by new call
+        new_env = {}
+        new_env["BUNDLER_ORIG_MANPATH"] = nil
+        new_env["BUNDLER_ORIG_PATH"] = nil
+        new_env["BUNDLER_VERSION"] = nil
+        new_env["BUNDLE_BIN_PATH"] = nil
+        new_env["RUBYLIB"] = nil
+        new_env["RUBYOPT"] = nil
+        
+        # DLM: preserve GEM_HOME and GEM_PATH set by current bundle because we are not supporting bundle
+        # requires to ruby gems will work, will fail if we require a native gem
+        #new_env["GEM_PATH"] = nil
+        #new_env["GEM_HOME"] = nil
+        
+        # DLM: for now, ignore current bundle in case it has binary dependencies in it
+        #bundle_gemfile = ENV['BUNDLE_GEMFILE']
+        #bundle_path = ENV['BUNDLE_PATH']    
+        #if bundle_gemfile.nil? || bundle_path.nil?
+          new_env['BUNDLE_GEMFILE'] = nil
+          new_env['BUNDLE_PATH'] = nil
+        #else
+        #  new_env['BUNDLE_GEMFILE'] = bundle_gemfile
+        #  new_env['BUNDLE_PATH'] = bundle_path    
+        #end  
+        
+        return new_env
+      end
+      
+      def run_command(command)
+        stdout_str, stderr_str, status = Open3.capture3(get_run_env(), command)
+        if status.success?
+          puts "Command completed successfully"
+          puts "stdout: #{stdout_str}"
+          puts "stderr: #{stderr_str}"
+          return true
+        else
+          puts "Error running command: '#{command}'"
+          puts "stdout: #{stdout_str}"
+          puts "stderr: #{stderr_str}"
+          return false 
+        end
+      end
 
       # test measures of calling gem with OpenStudio CLI system call
       def test_measures_with_cli
@@ -15,7 +63,7 @@ module OpenStudio
         puts "measures path: #{measures_dir}"
         gem_path = `gem environment gempath`
         gem_path = gem_path.split(':')[0]
-        gem_path = gem_path + '/gems'
+        gem_path = File.join(gem_path, 'gems')
         puts "GEM PATH: #{gem_path}"
 
         File.delete('Gemfile.lock') if File.exist?('Gemfile.lock')
@@ -26,12 +74,17 @@ module OpenStudio
         test_gems_path = @path + '/test_gems'
         system "bundle install --path #{test_gems_path}"
         system 'bundle lock --add_platform ruby'
+        
+        cli = OpenStudio.getOpenStudioCLI
+        puts "CLI: #{cli}"
 
-        the_call = "openstudio --verbose --bundle Gemfile --bundle_path #{test_gems_path} measure -r #{measures_dir}"
+        the_call = "#{cli} --verbose --bundle Gemfile --bundle_path ./test_gems/ measure -r #{measures_dir}"
         puts "SYSTEM CALL:"
         puts the_call
-        system "#{the_call}"
+        STDOUT.flush
+        run_command(the_call)
         puts "DONE"
+        STDOUT.flush
       end
     end
   end

ee673459e91b807640d1ccc7a28939e78a2f5044
kflemin <katherine.fleming@nrel.gov>
2021-03-02 16:32:27 -0700

    night cycling change - for 0.4.x

--- lib/openstudio/extension/core/os_lib_model_generation.rb
+++ lib/openstudio/extension/core/os_lib_model_generation.rb
@@ -3248,6 +3248,14 @@ module OsLib_ModelGeneration
       end
     end
 
+    manager_night_cycles.each do |night_cycle|
+      unless night_cycle.empty?
+        night_cycle.setThermostatTolerance(1.9999)
+        night_cycle.setCyclingRunTimeControlType("Thermostat")
+        runner.registerInfo(" night_cycle == #{night_cycle}")
+      end
+    end
+
     # report final condition of model
     runner.registerFinalCondition("The building finished with #{model.getModelObjects.size} objects.")
 
